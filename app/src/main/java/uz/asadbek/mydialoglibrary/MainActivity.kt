5257ea946f5cf7259d1378b8fe523048cc28350c
Asadbek <aaminboyev2@gmail.com>
2024-02-29 10:11:18 +0300

    Initial commit

---
+++ app/src/main/java/uz/asadbek/mydialoglibrary/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.asadbek.mydialoglibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
